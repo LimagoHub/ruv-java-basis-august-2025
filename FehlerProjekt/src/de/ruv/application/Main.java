edea709d0da6f7d0d44a485f055a6d5d4ea47047
Jo Wagner <jowagner@limago.de>
2025-08-20 14:20:31 +0200

    initial commit

---
+++ FehlerProjekt/src/de/ruv/application/Main.java
@@ -0,0 +1,42 @@
+package de.ruv.application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			
+			int a, b , c;
+			
+			a = b = 0;
+			
+			c =  a / b; 
+			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Vielen Dank Sie haben ein kleines Programm echt gluecklich gemacht");
+	}
+	
+	static int foo() {
+		try {
+			
+			int a, b , c;
+			
+			a = b = 0;
+			
+			return  a / b; 
+			
+		} catch (Exception e) {
+			return 0;
+		} finally {
+			System.out.println("Vielen Dank Sie haben ein kleines UnterProgramm echt gluecklich gemacht");
+		}
+		
+		//System.out.println("Vielen Dank Sie haben ein kleines UnterProgramm echt gluecklich gemacht");
+	}
+	
+	
+
+}
