bdddf7828f1e3ff5470d5755543bf1b58527b9e9
Jo Wagner <jowagner@limago.de>
2025-08-21 10:42:15 +0200

    initial commit

--- gui/src/de/gui/Fenster.java
+++ gui/src/de/gui/Fenster.java
@@ -3,25 +3,50 @@ package de.gui;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Fenster extends Frame{
+public class Fenster extends Frame implements KeyListener{
+	
+	private String message = "Hallo Fenster";
+	private int counter = 0;
 	
 	public Fenster() {
 		super("Ein tolles fenster");
 		setSize(500,500);
+		
+		addKeyListener(this);
+		
 		setVisible(true);
 	}
 	
 	@Override
 	public void paint(Graphics g) {
-		g.drawString("Hallo Fenster", 100, 100);
-		g.setColor(Color.PINK);
-		g.drawLine(100, 100, 200, 200);
+		g.drawString(message + " Counter = " + counter++, 100, 100);
+		
 	}
 
 	public static void main(String[] args) {
 		new Fenster();
 
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		message = "Taste wurde gedrueckt";
+		repaint();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
