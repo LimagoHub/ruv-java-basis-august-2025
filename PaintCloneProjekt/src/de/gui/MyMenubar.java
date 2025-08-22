f8c37ed67404368f67015a7575936ac7fd55402c
Jo Wagner <jowagner@limago.de>
2025-08-22 08:41:29 +0200

    initial commit

--- PaintCloneProjekt/src/de/gui/MyMenubar.java
+++ PaintCloneProjekt/src/de/gui/MyMenubar.java
@@ -1,36 +1,84 @@
 package de.gui;
 
+import java.awt.Color;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import static de.gui.PaintClone.Form;
 
 public class MyMenubar extends MenuBar {
 	
-	public MyMenubar() {
+	public MyMenubar(PaintClone paintclone) {
 		Menu menu;
 		MenuItem menuItem;
 		
 		menu = new Menu("Datei");
 		
 		menuItem = new MenuItem("Beenden");
+		
+		/*menuItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				paintclone.dispose();
+				
+			}
+		});*/
+		
+		menuItem.addActionListener(e->paintclone.dispose());
+		
 		menu.add(menuItem);
 		
 		add(menu);
 		
 		menu = new Menu("Formen");
 		
-		menuItem = new MenuItem("Linie");
+		menuItem = new MenuItem(Form.Linie.name());
+		menuItem.addActionListener(e->paintclone.setForm(Form.Linie));
 		menu.add(menuItem);
 		
 		menuItem = new MenuItem("Rechteck");
+		menuItem.addActionListener(e->paintclone.setForm(Form.Rechteck));
 		menu.add(menuItem);
 		
 		menuItem = new MenuItem("Oval");
+		menuItem.addActionListener(e->paintclone.setForm(Form.Oval));
 		menu.add(menuItem);
 		
 		menu.addSeparator();
 		
 		menuItem = new MenuItem("Loeschen");
+		menuItem.addActionListener(e->paintclone.setForm(Form.Loeschen));
+		menu.add(menuItem);
+		
+		add(menu);
+		
+		menu = new Menu("Farben");
+		
+		menuItem = new MenuItem("Weiss");
+		menuItem.addActionListener(e->paintclone.setFarbe(Color.WHITE));
+		menu.add(menuItem);
+		
+		menuItem = new MenuItem("Rot");
+		menuItem.addActionListener(e->paintclone.setFarbe(Color.RED));
+		menu.add(menuItem);
+		
+		menuItem = new MenuItem("Blau");
+		menuItem.addActionListener(e->paintclone.setFarbe(Color.BLUE));
+		menu.add(menuItem);
+		
+		menuItem = new MenuItem("Gelb");
+		menuItem.addActionListener(e->paintclone.setFarbe(Color.YELLOW));
+		menu.add(menuItem);
+		
+		menuItem = new MenuItem("Gruen");
+		menuItem.addActionListener(e->paintclone.setFarbe(Color.GREEN));
+		menu.add(menuItem);
+		
+		menuItem = new MenuItem("Pink");
+		menuItem.addActionListener(e->paintclone.setFarbe(Color.PINK));
 		menu.add(menuItem);
 		
 		add(menu);
--- PaintCloneProjekt/src/de/gui/PaintClone.java
+++ PaintCloneProjekt/src/de/gui/PaintClone.java
@@ -1,18 +1,46 @@
 package de.gui;
 
+import java.awt.Color;
 import java.awt.Frame;
+
 
 public class PaintClone extends Frame{
 
+	public enum Form {Linie, Rechteck, Oval, Loeschen}
 	private static final int SIZE = 500;
 	
+	private Color farbe = Color.WHITE;
+	private Form form = Form.Linie;
+	
+	
+	
+	public Color getFarbe() {
+		return farbe;
+	}
+
+	public void setFarbe(Color farbe) {
+		this.farbe = farbe;
+	}
+
+	public Form getForm() {
+		return form;
+	}
+
+	public void setForm(Form form) {
+		this.form = form;
+	}
+
 	public PaintClone() {
 		setSize(SIZE,SIZE);
-		setMenuBar(new MyMenubar());
+		setMenuBar(new MyMenubar(this));
 		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
+		
+		Form f = Form.Linie;
+		
+		
 		new PaintClone();
 
 	}
